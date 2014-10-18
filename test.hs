88d0746c05dd85ffc8272299ff4e57f66e54844e
BartAdv <bprzygoda@gmail.com>
2014-10-18 00:35:03 +0000

    Obligatory initial commit

---
+++ test.hs
@@ -0,0 +1,132 @@
+import Control.Concurrent (threadDelay)
+import Control.Monad
+import Pipes
+import Pipes.Concurrent
+import qualified Pipes.Prelude as P
+import Data.Monoid
+import System.Console.ANSI
+import Debug.Trace
+
+-- this is all very quick n' dirty, just to play with composing
+-- pipes and forkIO
+-- don't expect quality code
+-- you have been warned
+
+
+-- the flow of 'messages' is following:
+-- * 'input' subsystems produce commands
+-- * commands are processed by entities handlers
+-- * handlers are producing events
+-- * events are consumed by 'output' subsystems
+-- * events can be transformed to commands
+
+
+data Command = MoveLeft | MoveDown | MoveUp | MoveRight
+             | MakeAttack     -- no info on target
+             | MakeDamage Int -- hence we're gonna only damage ourselves
+             | DisplayInfo
+               
+data Event = Redraw Char Int Int Int Int
+           | DrawInfo String
+           | Attack Int Int
+           | Damage Int
+
+renderer :: Consumer Event IO ()
+renderer = forever $ do
+  ev <- await
+  case ev of
+    Redraw c ox oy nx ny -> lift $ do
+        setCursorPosition ox oy
+        putChar ' '
+        setCursorPosition nx ny
+        putChar c
+    DrawInfo msg -> lift $ do
+      setCursorPosition 20 0
+      putStr msg
+    Attack _ _ -> lift $ do
+        setCursorPosition 20 0
+        putStr "Attack!"
+    Damage _ -> lift $ do
+        setCursorPosition 20 0
+        putStr "Splash!"    
+
+input :: Producer Command IO r
+input = forever $ do
+  k <- lift getChar
+  case k of
+    'h' -> yield MoveLeft
+    'j' -> yield MoveDown
+    'k' -> yield MoveUp
+    'l' -> yield MoveRight
+    'a' -> yield MakeAttack
+    'i' -> yield DisplayInfo
+    _ -> return ()
+
+data Entity = Entity { posX   :: Int
+                     , posY   :: Int
+                     , avatar :: Char
+                     , target :: (Int, Int)
+                     , health :: Int }
+              deriving(Show)
+
+handler :: Entity -> Pipe Command Event IO ()
+handler ent = loop ent
+  where
+    loop Entity{posX=x, posY=y, avatar=av, target=(tx,ty), health=h} = do
+      when (h > 0) $ do
+        cmd <- await
+        let (tx,ty) = case cmd of
+              MoveLeft  -> (0, -1)
+              MoveUp    -> (-1, 0)
+              MoveDown  -> (1, 0)
+              MoveRight -> (0, 1)
+              _ -> (0,0)
+            (nx,ny) = (x+tx, y+ty)
+            nh = case cmd of
+              MakeDamage x -> h - x
+              _ -> h
+        case cmd of
+          MakeAttack -> yield $ Attack tx ty
+          DisplayInfo -> yield $ DrawInfo $ (show h)
+          _ -> yield $ Redraw av x y nx ny
+        loop $ Entity nx ny av (nx+tx, ny+ty) nh
+
+-- an example of subsystem that just processes events
+combat :: Pipe Event Event IO ()
+combat = forever $ do
+  cmd <- await
+  case cmd of
+    Attack x y -> do
+      lift $ putStr "!"
+      yield $ Damage 10
+    _ -> return ()
+
+-- not all 'output' subsystems are busy producing effects in IO
+-- some just want to produce commands for further processing
+-- hence loopback will be wired again to input
+loopback :: Pipe Event Command IO ()
+loopback = forever $ do
+  ev <- await
+  case ev of
+    Damage x -> yield $ MakeDamage x
+    _ -> return ()
+    
+main = do
+  clearScreen
+  let player = Entity 0 0 '@' (0,1) 100
+
+  -- this is where commands are coming from
+  (cmdOut, cmdIn) <- spawn Unbounded
+  -- and this is where events are born
+  (evOut, evIn) <- spawn Unbounded
+
+  -- input subsystem - push the input commands
+  forkIO $ do runEffect $ input >-> toOutput cmdOut
+              performGC
+  -- handling subsystem
+  forkIO $ do runEffect $ fromInput cmdIn >-> handler player >-> toOutput evOut
+              performGC
+  -- here we are transfering events from output to the input
+  -- for the 'logic' subsystems that work in such manner
+  forkIO $ do runEffect $ fromInput evIn >-> combat >-> loopback >-> toOutput cmdOut
+  runEffect $ fromInput evIn >-> renderer
